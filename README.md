1979ac5e95bac78b0c9177781094ec569ff220b9
Prasanna <prsna003@gmail.com>
2024-02-29 14:06:35 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # My-Complete-Project
 In this project, i'm gonna work in html,css,js, scss, reactjs,redux, nodejs, nextjs, expressjs, mongodb, node socket with chat app and etc.
+
+# References
+https://www.freecodecamp.org/news/nextjs-tutorial/ 
---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json({ status: true, message: "Backend Server is Running" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend Server is Running in http://localhost:${PORT} `);
+});
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/MyCompleteProject")
+  .then((connected) => {
+    console.log("Mongodb Connected Successfully");
+  })
+  .catch((err) => {
+    console.log("Mongodb Connection Error:", err);
+  });
